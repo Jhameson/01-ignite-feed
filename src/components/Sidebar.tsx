5e426ff3f1e47b7da737d3414e5d247cb5d9d05e
jhameson <jhamesonlucas22@gmail.com>
2023-06-25 12:57:14 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,25 @@
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
-import profile from '../assets/Profile.png'
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img src={profile} alt="" className={styles.cover} />
+      <img
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+        alt=""
+        className={styles.cover}
+      />
       <div className={styles.profile}>
+        <img src="https://github.com/jhameson.png" alt="" />
         <strong>Jhameson</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
